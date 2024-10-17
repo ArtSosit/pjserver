ff13f37f214efa9cc0e6e0eb252ade7abdfdf64e
“ArtSosit” <artsonon@gmail.com>
2024-10-17 12:07:15 +0700

    changeip

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/tables", tableRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  // console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
