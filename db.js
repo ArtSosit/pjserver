7a92f9fbb72d258bd60e5fd777df86e41c34d6d7
“ArtSosit” <artsonon@gmail.com>
2024-10-17 12:21:26 +0700

    .

--- db.js
+++ db.js
@@ -1,18 +1,22 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "100.112.89.18",
   user: "arty",
   password: "00000000",
   database: "orderfoodonline",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to MySQL:", err);
     return;
   }
   console.log("Connected to MySQL");
+  connection.release(); // ปล่อยการเชื่อมต่อหลังจากใช้งาน
 });
 
-module.exports = connection;
+module.exports = pool;
