4b837a70c8e6ce712bd23a53f2db5db2af3f66e6
“ArtSosit” <artsonon@gmail.com>
2025-02-24 11:25:12 +0700

    t

--- db.js
+++ db.js
@@ -1,16 +1,23 @@
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  host: "10.160.47.133",
-  user: "arty",
-  password: "00000000",
-  database: "orderfoodonline",
-  port: 3306,
-});
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
-  }
-  console.log("Connected to MySQL");
-});
-module.exports = connection;
+// const mysql = require("mysql2");
+// const connection = mysql.createConnection({
+//   host: "10.160.47.133",
+//   user: "arty",
+//   password: "00000000",
+//   database: "orderfoodonline",
+//   port: 3306,
+// });
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to MySQL:", err);
+//     return;
+//   }
+//   console.log("Connected to MySQL");
+// });
+// module.exports = connection;
+
+import postgres from "postgres";
+
+const connectionString = process.env.DATABASE_URL;
+const sql = postgres(connectionString);
+
+export default sql;
